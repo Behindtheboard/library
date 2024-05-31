5da6dbfbcbb2160a9aa59d883bc1208330d070e3
Behindtheboard <jk2624@gmail.com>
2024-05-30 23:05:50 -0700

    Setup skeleton html css and js

---
+++ library_script.js
@@ -0,0 +1,16 @@
+const myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    return `${title} by ${author}, ${pages}, ${read}`;
+}
+
+function addBookToLIbrary(book) {
+    myLibrary.push(book)
+}
