0e506b0e9bb50422b9dc589c9e0269590221d7cf
Behindtheboard <jk2624@gmail.com>
2024-06-01 13:22:28 -0700

    Add function that creates cards with DOM

--- library_script.js
+++ library_script.js
@@ -5,13 +5,41 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    createCard(this);
+}
+function createCard(book) {
+    const bookshelf = document.querySelector("#bookshelf");
+
+    let book = bookshelf.createElement('div');
+    book.classList.add("book-card");
+    book.setAttribute('id',`${book}Card`)
+    bookshelf.appendChild(book);
+    
+    const bookTitle = book.createElement('div');
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = `${book.title}`;
+    book.appendChild(bookTitle);
+
+    const bookAuthor = book.createElement('div');
+    bookAuthor.classList.add("book-author");
+    bookAuthor.textContent = `${book.author}`;
+    book.appendChild(bookAuthor);
+
+    const bookPages = book.createElement('div');
+    bookPages.classList.add("book-pages");
+    bookPages.textContent = `${book.pages}`;
+    book.appendChild(bookPages);
+}
+
+function addBookToLIbrary(book) {
+    myLibrary.push(book);
 }
 
 const book1 = new Book("Harry Potter", "J.K. Rowling", "500 pages", "read");
 const book2 = new Book("Eragon", "Christopher Wild", "600 Pages", "read");
 const book3 = new Book("Star Wars", "George Lucas", "1000 Pages", "unread");
 
-function addBookToLIbrary(book) {
-    myLibrary.push(book)
-}
-
+addBookToLIbrary(book1);
+addBookToLIbrary(book2);
+console.log(myLibrary);
+console.log(book1.title)
